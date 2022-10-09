e8c0dfb4e85df40e5e4277b2f9a2e4a075ae54a3
Ivan <103798094@student.swin.edu.au>
2022-10-09 23:30:28 +1100

    Receipt Functional with some styling

--- assign2/receipt.php
+++ assign2/receipt.php
@@ -30,26 +30,7 @@ else{
         $record = mysqli_fetch_assoc($result);
             echo '<div class = "MyContainer">
             <center class = "InvoiceTop"><h2>Order Successful!!</h2></center>';
-            echo '<div class = MidInvoice"> <h2>Contact Info</h2> <p> Address :',$record["cust_Street"],"</br>";
-            echo  ' Name : ',$record["cust_Fname"] ," ", $record["cust_Lname"], "</br>";
-            echo  ' Email : ',$record["cust_email"], "</br>";
-            echo '</p></div>';
-            echo "<table border='1'>\n";
-            echo "<tr>\n
-                        <th scope='col'>Order ID</th>
-                        <th scope='col'>Ticket Type</th>
-
-                        <th scope='col'>Quantity</th>
-                        <th scope='col'>Price</th>
-                  </tr>\n";
-                  echo "<tr>\n";
-					echo "<td>", $record["order_id"], "</td>\n";
-                    echo "<td>", $record["ticket_type"], "</td>\n";
-					echo "<td>", $record["Quantity"], "</td>\n";
-					echo "<td>", $record["order_cost"], "</td>\n";
-					echo "</tr>\n";
-                    echo "</table>\n";
-
+           
             echo '</div>';
             
       
@@ -66,14 +47,14 @@ else{
     			<div class="receipt-header">
 					<div class="col-xs-6 col-sm-6 col-md-6">
 						<div class="receipt-left">
-							<img class="img-responsive" alt="iamgurdeeposahan" src="https://bootdey.com/img/Content/avatar/avatar6.png" style="width: 71px; border-radius: 43px;">
+							<img class="img-responsive"  src='./images/preview/Logo.png'  style="width: 91px; border-radius: 50px;">
 						</div>
 					</div>
 					<div class="col-xs-6 col-sm-6 col-md-6 text-right">
 						<div class="receipt-right">
-							<h5>Company Name.</h5>
-							<p>+1 3649-6589 <i class="fa fa-phone"></i></p>
-							<p>company@gmail.com <i class="fa fa-envelope-o"></i></p>
+							<h3>Gang Of Islands</h3>
+							<p>Replace with PhoneNumber <i class="fa fa-phone"></i></p>
+							<p>GOISupport@hotmail.com<i class="fa fa-envelope-o"></i></p>
 							<p>USA <i class="fa fa-location-arrow"></i></p>
 						</div>
 					</div>
@@ -84,15 +65,15 @@ else{
 				<div class="receipt-header receipt-header-mid">
 					<div class="col-xs-8 col-sm-8 col-md-8 text-left">
 						<div class="receipt-right">
-							<h5>Customer Name </h5>
+							<h5>Customer Name : <?php echo  $record["cust_Fname"] ," ", $record["cust_Lname"], "</br>"; ?></h5>
 							<p><b>Mobile :</b> +1 12345-4569</p>
-							<p><b>Email :</b> customer@gmail.com</p>
-							<p><b>Address :</b> New York, USA</p>
+							<p><b>Email :</b> <?= $record["cust_email"] ?></p>
+							<p><b>Address :</b><?= $record["cust_Street"]?></p>
 						</div>
 					</div>
 					<div class="col-xs-4 col-sm-4 col-md-4">
 						<div class="receipt-left">
-							<h3>INVOICE # 102</h3>
+							<h3>INVOICE</h3>
 						</div>
 					</div>
 				</div>
@@ -103,56 +84,27 @@ else{
                     <thead>
                         <tr>
                             <th>Description</th>
-                            <th>Amount</th>
+                            <th>Quantity</th>
                         </tr>
                     </thead>
                     <tbody>
                         <tr>
-                            <td class="col-md-9">Payment for August 2016</td>
-                            <td class="col-md-3"><i class="fa fa-inr"></i> 15,000/-</td>
+                            <td class="col-md-9"><?=$record["ticket_type"]?></td>
+                            <td class="col-md-3"><i class="fa fa-inr"></i><?= $record["Quantity"] ?></td>
                         </tr>
-                        <tr>
-                            <td class="col-md-9">Payment for June 2016</td>
-                            <td class="col-md-3"><i class="fa fa-inr"></i> 6,00/-</td>
-                        </tr>
-                        <tr>
-                            <td class="col-md-9">Payment for May 2016</td>
-                            <td class="col-md-3"><i class="fa fa-inr"></i> 35,00/-</td>
-                        </tr>
+                     
                         <tr>
                             <td class="text-right">
-                            <p>
-                                <strong>Total Amount: </strong>
-                            </p>
-                            <p>
-                                <strong>Late Fees: </strong>
-                            </p>
-							<p>
-                                <strong>Payable Amount: </strong>
-                            </p>
-							<p>
-                                <strong>Balance Due: </strong>
-                            </p>
+                         
 							</td>
                             <td>
-                            <p>
-                                <strong><i class="fa fa-inr"></i> 65,500/-</strong>
-                            </p>
-                            <p>
-                                <strong><i class="fa fa-inr"></i> 500/-</strong>
-                            </p>
-							<p>
-                                <strong><i class="fa fa-inr"></i> 1300/-</strong>
-                            </p>
-							<p>
-                                <strong><i class="fa fa-inr"></i> 9500/-</strong>
-                            </p>
+                        
 							</td>
                         </tr>
                         <tr>
                            
                             <td class="text-right"><h2><strong>Total: </strong></h2></td>
-                            <td class="text-left text-danger"><h2><strong><i class="fa fa-inr"></i> 31.566/-</strong></h2></td>
+                            <td class="text-left text-danger"><h2><strong><i class="fa fa-inr"></i> <?= $record["order_cost"]?></strong></h2></td>
                         </tr>
                     </tbody>
                 </table>
@@ -162,8 +114,8 @@ else{
 				<div class="receipt-header receipt-header-mid receipt-footer">
 					<div class="col-xs-8 col-sm-8 col-md-8 text-left">
 						<div class="receipt-right">
-							<p><b>Date :</b> 15 Aug 2016</p>
-							<h5 style="color: rgb(140, 140, 140);">Thanks for shopping.!</h5>
+							<p><b>Date :</b><?=$record["order_date"] ?></p>
+							<h5 style="color: rgb(140, 140, 140);">Thank you for your purchase!. We hope to see you again!</h5>
 						</div>
 					</div>
 					<div class="col-xs-4 col-sm-4 col-md-4">

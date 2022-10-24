a7dd1147d159190d0120ee1be82ce230b6d56c0b
Aweb Mekhilef <awebmekhilef@gmail.com>
2022-10-24 15:59:32 +1100

    Deleting an order

--- assign2/manager.php
+++ assign2/manager.php
@@ -23,16 +23,27 @@ $res = $conn->query("select * from s103574757_db.orders o inner join s103574757_
             <th>Last name</th>
             <th>Status</th>
             <th>Product</th>
+            <th>Action</th>
         </tr>
         
+        <?php while ($row = mysqli_fetch_assoc($res)) { ?>
         <tr>
-            <?php while ($row = mysqli_fetch_assoc($res)) { ?>
                 <td><?php echo $row['order_id']; ?></td>
                 <td><?php echo $row['order_cost']; ?></td>
                 <td><?php echo $row['first_name']; ?></td>
                 <td><?php echo $row['last_name']; ?></td>
                 <td><?php echo $row['order_status']; ?></td>
                 <td><?php echo $row['movie_name']; ?></td>
+                <td>
+                    <!-- Send the user to the edit page (which fetches order data via order_id) -->
+                    <a href="edit.php?id=<?php echo $row['order_id']?>">Edit</a>
+
+                    <!-- Delete order by posting to delete_order.php. We need to use a form because JS is not allowed :( -->
+                    <form action="delete_order.php" method="post">
+                        <input type="hidden" name="id" value="<?php echo $row['order_id'] ?>">
+                        <input type="submit" value="Delete">
+                    </form>
+                </td>
             <?php } ?>
         </tr>
     </table>

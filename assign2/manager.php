d3862c2248e9b4e2e52bac372898df32428f8592
Aweb Mekhilef <awebmekhilef@gmail.com>
2022-10-24 15:48:15 +1100

    Join movie table on order in manager

--- assign2/manager.php
+++ assign2/manager.php
@@ -2,7 +2,7 @@
 
 require_once("db.php");
 
-$res = $conn->query("select * from s103574757_db.orders");
+$res = $conn->query("select * from s103574757_db.orders o inner join s103574757_db.movies m on o.movie_id=m.movie_id order by o.order_cost desc;");
 
 ?>
 
@@ -18,17 +18,21 @@ $res = $conn->query("select * from s103574757_db.orders");
     <table>
         <tr>
             <th>ID</th>
+            <th>Total cost</th>
             <th>First name</th>
             <th>Last name</th>
             <th>Status</th>
+            <th>Product</th>
         </tr>
         
         <tr>
             <?php while ($row = mysqli_fetch_assoc($res)) { ?>
                 <td><?php echo $row['order_id']; ?></td>
-                <td><?php echo $row['firstname']; ?></td>
-                <td><?php echo $row['lastname']; ?></td>
+                <td><?php echo $row['order_cost']; ?></td>
+                <td><?php echo $row['first_name']; ?></td>
+                <td><?php echo $row['last_name']; ?></td>
                 <td><?php echo $row['order_status']; ?></td>
+                <td><?php echo $row['movie_name']; ?></td>
             <?php } ?>
         </tr>
     </table>

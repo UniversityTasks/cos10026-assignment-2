dd5a485a7baa5efab9b4499485222f68e6ce3852
Mayer Cook-Tonkin <s104249535@student.swinburne.edu.au>
2022-10-28 15:54:40 +1100

    Added manager login and authentication plus css

---
+++ assign2/authentication.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <meta charset="utf-8" />
+        <meta name="description" content="GOI Cinemas - Authentication" />
+        <meta name="keywords" content="HTML,CSS,Javascript" />
+        <meta name="author" content="Gang of Islands" />
+        <link rel="stylesheet" href="./styles/style.css">
+    </head>
+    <body>
+        <?php
+            require_once("./db.php");
+            require_once("./functions/functions.php");
+
+            // If the users table doesn't already exists, create it.
+            // We can check if a table exists be selecting a single entry from it
+            // However, on some setups this causes an exception so we handle it to be safe
+            try {                
+                if ($conn->query('select * from s103574757_db.users limit 1') == false) {
+                    // Go to the catch block which contains the creation query
+                    throw new Exception();
+                }
+            } catch (Exception $e) {
+                $query = "CREATE TABLE s103574757_db.users (
+                    id int(10) AUTO_INCREMENT,
+                    username varchar(50) NOT NULL,
+                    password varchar(50) NOT NULL
+                )";
+            }
+
+            if (isset($_GET["username"]) && isset($_GET['password'])) {
+                $username = sanitise_input($_GET["username"]);
+                $password = sanitise_input($_GET["password"]);
+
+                $user_query_sql= "SELECT * FROM s103574757_db.users WHERE username = '$username' AND password = '$password';";
+                $result = $conn->query($user_query_sql);
+
+                $user_detail = mysqli_fetch_assoc($result);
+                if (sizeof($user_detail) == 0) {
+                    header("Location: ./login_form.php");
+                } else {
+                    header("Location: ./manager.php");
+                }
+            }
+        ?>
+    </body>
+</html>
---
+++ assign2/login_form.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <meta charset="utf-8" />
+        <meta name="description" content="GOI Cinemas - Manager Login" />
+        <meta name="keywords" content="HTML,CSS,Javascript" />
+        <meta name="author" content="Gang of Islands" />
+        <link rel="stylesheet" href="./styles/style.css">
+    </head>
+
+    <body>
+        <?php include_once 'includes/menu.php'; ?>
+
+        <div id="loginContainer">
+            <form id="loginForm" method='get' action="authentication.php">
+                <fieldset class="formFieldset">
+                    <legend>Manager Login</legend>
+
+                    <div class="formGroup">
+                        <label for="username">Username: </label>
+                        <input type="text" name="username" id="username" pattern="[A-Za-z]{1,25}" placeholder="Username" required />
+                    </div>
+
+                    <div class="formGroup">
+                        <label for="password">Password: </label>
+                        <input type="password" name="password" id="password" pattern="[A-Za-z]{1,25}" placeholder="Password" required />
+                    </div>
+                   
+                    <div class="loginSubmitBtn">
+                    <input type="submit" value="Login">
+                    </div>
+            </fieldset>
+
+            </form>
+        </div>
+                
+        <?php include_once 'includes/footer.php'; ?>
+    </body>
+            
+</html>
